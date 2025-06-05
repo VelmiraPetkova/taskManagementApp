9e0969b4fb2bd711b773a86f03c9d9fadbe7a4fc
velmira.georgieva <velmira.cacc@gmail.com>
2025-06-05 08:34:26 +0300

    Create Docker containers for the Front-end application and Python backend,

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'http://127.0.0.1:5001';
 
 const registerForm = document.getElementById('register-form');
 const loginForm = document.getElementById('login-form');
